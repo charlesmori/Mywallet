55e0d01fa5ca018ac6814d0450517fa953bfbef0
Charles Mori <charlesmori09@gmail.com>
2024-12-09 20:10:53 +0200

    reversing back the route for the login button

--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -70,7 +70,7 @@ class _LoginPageState extends State<LoginPage> {
                   context,
                   MaterialPageRoute(
                     // builder: (context) => const HomePage(),
-                    builder: (context) => const DashboardPage(),
+                    builder: (context) => const HomePage(),
                   ),
                 );
               },
